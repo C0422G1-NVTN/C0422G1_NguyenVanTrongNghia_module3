ea579a4842b000ff6b5c99eb811c5a7a902d0e62
C0422G1-Nguyễn Văn Trọng Nghĩa <nghianf@gmail.com>
2022-07-29 08:14:34 +0700

    update loi reviewing

--- ss10_JSP_JSTL/bai_tap/baitap2_ung_dung_calculator/src/main/java/servlet/CalculatorServlet.java
+++ ss10_JSP_JSTL/bai_tap/baitap2_ung_dung_calculator/src/main/java/servlet/CalculatorServlet.java
@@ -1,6 +1,6 @@
 package servlet;
 
-import model.Calculator;
+import service.CalculatorService;
 
 import javax.servlet.*;
 import javax.servlet.http.*;
@@ -16,17 +16,17 @@ public class CalculatorServlet extends HttpServlet {
 
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-double firstNumber = Double.parseDouble(request.getParameter("firstNumber"));
-double secondNumber = Double.parseDouble(request.getParameter("secondNumber"));
-String operator = request.getParameter("operator");
+        double firstNumber = Double.parseDouble(request.getParameter("firstNumber"));
+        double secondNumber = Double.parseDouble(request.getParameter("secondNumber"));
+        String operator = request.getParameter("operator");
 
-String result = Calculator.calculator(firstNumber,secondNumber,operator);
-RequestDispatcher requestDispatcher = request.getRequestDispatcher("/display.jsp");
-request.setAttribute("firstNumber",firstNumber);
-request.setAttribute("operator",operator);
-request.setAttribute("secondNumber",secondNumber);
-request.setAttribute("result",result);
+        String result = CalculatorService.calculator(firstNumber, secondNumber, operator);
+        RequestDispatcher requestDispatcher = request.getRequestDispatcher("/display.jsp");
+        request.setAttribute("firstNumber", firstNumber);
+        request.setAttribute("operator", operator);
+        request.setAttribute("secondNumber", secondNumber);
+        request.setAttribute("result", result);
 
-requestDispatcher.forward(request,response);
+        requestDispatcher.forward(request, response);
     }
 }
